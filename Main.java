865fb6d528dfc22ae9d4518f37b846efd3bb7e5e
Larasati Khadijah <larasatikh31@gmail.com>
2023-10-28 15:00:11 +0700

    update program

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo");
+        Scanner input = new Scanner(System.in);
+
+        // Input panjang sisi persegi dari pengguna
+        System.out.print("Masukkan panjang sisi persegi: ");
+        double sisi = input.nextDouble();
+
+        // Hitung luas persegi
+        double luas = sisi * sisi;
+
+        // Tampilkan hasil
+        System.out.println("Luas dari persegi adalah: " + luas);
     }
 }
